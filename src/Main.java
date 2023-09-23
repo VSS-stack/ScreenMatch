7847cabd5c20d0303e48d7c2cf6ffa9cfca04679
VSS-stack <victor.stsilva93@gmail.com>
2023-09-23 14:59:50 -0300

    Instantianting an object

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-
 public class Main {
     public static void main(String[] args) {
+        Film myFilm = new Film();
+        myFilm.name = "The Godfather";
+        myFilm.releaseYear = 1972;
+        myFilm.durationInMinutes = 175;
 
-        }
+        System.out.println(myFilm.name);
+        System.out.println(myFilm.releaseYear);
     }
 }
