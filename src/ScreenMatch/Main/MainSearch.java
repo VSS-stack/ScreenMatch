5efa847f7921fbc04a25095839f2ab9e9508c1a5
VSS-stack <victor.stsilva93@gmail.com>
2023-11-22 21:45:25 -0300

    Starting http request

---
+++ src/ScreenMatch/Main/MainSearch.java
@@ -0,0 +1,20 @@
+package ScreenMatch.Main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class MainSearch {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=306f71f9"))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
