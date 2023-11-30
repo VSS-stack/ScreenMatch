5d5029b7a08ab57611b72b7a216b547cf0817bf5
VSS-stack <victor.stsilva93@gmail.com>
2023-11-29 21:27:31 -0300

    Improving the request and using regex

--- src/ScreenMatch/Main/MainSearch.java
+++ src/ScreenMatch/Main/MainSearch.java
@@ -5,14 +5,22 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Write the name of the film:");
+        var search = input.nextLine().replaceAll("\\s", "+"); //regex to change empty spaces to "+"
+
+        /* Do a GET request */
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=306f71f9"))
+                .uri(URI.create("http://www.omdbapi.com/?t=" + search + "&apikey=306f71f9"))
                 .build();
 
+        /* Handle the response */
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
