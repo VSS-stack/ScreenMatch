34f9e8ea7664724ac4d6e89f68ce335f166f3264
VSS-stack <victor.stsilva93@gmail.com>
2023-12-28 12:09:16 -0300

    Transfoming an arraylist into a json

--- src/ScreenMatch/Main/MainSearch.java
+++ src/ScreenMatch/Main/MainSearch.java
@@ -13,45 +13,72 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner input = new Scanner(System.in);
+        String search = "";
+        List<Title> titles = new ArrayList<>();
 
-        System.out.println("Write the name of the film:");
-        var search = input.nextLine().replaceAll("\\s", "+"); //regex to change empty spaces to "+"
+        /* Create a Gson object */
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting() //Print the json in a more visual way
+                .create();
 
-        /* Do a GET request */
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=" + search + "&apikey=306f71f9"))
-                .build();
+        while (!search.equalsIgnoreCase("exit")) {
+            System.out.println("Write the name of the film:");
+            search = input.nextLine().replaceAll("\\s", "+"); //regex to change empty spaces to "+"
 
-        /* Handle the response */
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+            /* Exit the loop */
+            if (search.equalsIgnoreCase("exit")) {
+                break;
+            }
 
-        /* Transform the response into an object */
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        TitleOmbd myTitleOmdb = gson.fromJson(response.body(), TitleOmbd.class);
-        System.out.println(myTitleOmdb);
+            /* Do a GET request */
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://www.omdbapi.com/?t=" + search + "&apikey=306f71f9"))
+                    .build();
 
-        try {
-            /* Convert into a Title object */
-            Title myTitle = new Title(myTitleOmdb);
-            System.out.println("Converted title:");
-            System.out.println(myTitle);
+            /* Handle the response */
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
 
-            /* Write a file with the title atributes */
-            FileWriter newFile = new FileWriter("Movies.txt");
-            newFile.write(myTitle.toString());
-            newFile.close();
-        } catch (NumberFormatException e) {
-            System.out.println("An error occurred: " + e.getMessage());
-        } catch (YearConversionException e) {
-            System.out.println("An error occurred: " + e.getMessage());
+            /* Transform the response into an object */
+            TitleOmbd myTitleOmdb = gson.fromJson(response.body(), TitleOmbd.class);
+            System.out.println(myTitleOmdb);
+
+            try {
+                /* Convert into a Title object */
+                Title myTitle = new Title(myTitleOmdb);
+                System.out.println("Converted title:");
+                System.out.println(myTitle);
+
+//                /* Write a file with the title atributes */
+//                FileWriter newFile = new FileWriter("Movies.txt");
+//                newFile.write(myTitle.toString());
+//                newFile.close();
+
+                /* Add title to list */
+                titles.add(myTitle);
+
+            } catch (NumberFormatException e) {
+                System.out.println("An error occurred: " + e.getMessage());
+            } catch (YearConversionException e) {
+                System.out.println("An error occurred: " + e.getMessage());
+            }
         }
+        System.out.println(titles);
+
+        /* Write a file with a Json */
+        FileWriter myFile = new FileWriter("Titles.json");
+        myFile.write(gson.toJson(titles));
+        myFile.close();
+
+        System.out.println("The program ended correctly");
     }
 }
